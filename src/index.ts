d5c886b6f4d551b3556f970e3e01c8ca7d348713
groone38 <groone38@yandex.ru>
2023-07-01 15:07:06 +0300

    testing

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
 import mongoose from "mongoose";
-import { authRouter, userRouter } from "./src/router/index";
+import { authRouter, userRouter } from "./router/index";
 
 require("dotenv").config();
 const app = express();
@@ -26,11 +26,11 @@ server.listen(8080, () => {
   console.log("Server running on http//localhost:8080/");
 });
 
-// const MONGO_URL =
-//   "mongodb+srv://admin:admin@cluster0.l0s3o2l.mongodb.net/?retryWrites=true&w=majority";
+const MONGO_URL =
+  "mongodb+srv://admin:admin@cluster0.l0s3o2l.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.Promise = Promise;
-mongoose.connect(process.env.MONGODB_URL);
+mongoose.connect(MONGO_URL);
 mongoose.connection.on("error", (error: Error) => console.log(error));
 
 app.use("/auth", authRouter);
