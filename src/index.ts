e17da255d91fe49ef30f725863c93ea27dbdbcb5
groone38 <groone38@yandex.ru>
2023-07-03 23:30:48 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://privat-auth.vercel.app"],
-    methods: ["POST", "GET", "PUT", "DELETE"],
-    credentials: true,
+    origin: "*",
+    // origin: ["https://privat-auth.vercel.app"],
+    // methods: ["POST", "GET", "PUT", "DELETE"],
+    // credentials: true,
   })
 );
 app.use("/image", express.static("image"));
